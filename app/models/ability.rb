6c7824fe98beeae6642f323f83c4626a035b3a44
Elhambasir <elham1378basir@gmail.com>
2023-08-09 14:34:38 +0430

    Use cancancan for recipe

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,10 @@ class Ability
 
   def initialize(user)
     can :read, Food
+    can :read, Recipe
     return unless user.present?
 
     can :manage, Food, user_id: user.id
+    can :manage, Recipe, user_id: user.id
   end
 end
