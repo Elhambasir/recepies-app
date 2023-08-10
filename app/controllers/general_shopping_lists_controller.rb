ea8507f3b910d44f999c7585828c3b21e53621d6
Elhambasir <elham1378basir@gmail.com>
2023-08-10 19:10:00 +0430

    Update the general-shopping-list index action

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,7 +1,11 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @food_lists = Food.joins(:recipe_foods).where(foods: { user_id: 1 }).where('foods.quantity < recipe_foods.quantity')
-      .select('foods.*, (recipe_foods.quantity -foods.quantity) as difference')
+    # @food_lists = Food.joins(:recipe_foods).where(foods: { user_id: current_user.id }).where('foods.quantity < recipe_foods.quantity')
+    #   .select('foods.*, (recipe_foods.quantity -foods.quantity) as difference')
+      
+    @recipes = current_user.recipes.includes(recipe_foods: :food)
+    @all_recipe_foods = @recipes.map(&:recipe_foods).flatten
+    @food_lists = @all_recipe_foods.select { |recipe| recipe.quantity > recipe.food.quantity }
     @total_price = total_price_of_missing_food(@food_lists)
   end
 
@@ -10,7 +14,7 @@ class GeneralShoppingListsController < ApplicationController
   def total_price_of_missing_food(food_list)
     total_price = 0
     food_list.each do |food|
-      total_price += food.price * food.difference
+      total_price += food.food.price * food.food.quantity
     end
     total_price
   end
