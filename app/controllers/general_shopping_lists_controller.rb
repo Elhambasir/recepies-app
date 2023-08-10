509aa4da29621092593456faf65fd3e6b3647c87
Elhambasir <elham1378basir@gmail.com>
2023-08-10 23:11:02 +0430

    Update general_shopping_lists_controller.rb
    Add function to get all food and total price ...

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,7 +1,18 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @food_lists = Food.joins(:recipe_foods).where(foods: { user_id: 1 }).where('foods.quantity < recipe_foods.quantity')
-      .select('foods.*, (recipe_foods.quantity -foods.quantity) as difference')
+    @foods = Food.joins(:recipe_foods)
+      .where(foods: { user_id: current_user.id })
+      .where('foods.quantity < recipe_foods.quantity')
+      .distinct
+    @food_lists = @foods.map do |food|
+      {
+        name: food.name,
+        measurement_unit: food.measurement_unit,
+        quantity: food.quantity,
+        total_quantity: food.recipe_foods.sum(:quantity) - food.quantity,
+        price: (food.recipe_foods.sum(:quantity) - food.quantity) * food.price
+      }
+    end
     @total_price = total_price_of_missing_food(@food_lists)
   end
 
@@ -10,7 +21,7 @@ class GeneralShoppingListsController < ApplicationController
   def total_price_of_missing_food(food_list)
     total_price = 0
     food_list.each do |food|
-      total_price += food.price * food.difference
+      total_price += food[:price]
     end
     total_price
   end
