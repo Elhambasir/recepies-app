1c16681c3784dd3c4873be89a36a4528c3043723
Elhambasir <elham1378basir@gmail.com>
2023-08-10 08:22:28 +0430

    Add total and number of food items in public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).includes(:user).order(created_at: :desc)
+    @users = User.all
+    @public_recipes = Recipe.includes(%i[recipe_foods user]).where(public: true).order(created_at: :desc)
   end
 end
