ad620e1777a151f6a736326a9d11419350b66095
Elhambasir <elham1378basir@gmail.com>
2023-08-10 15:16:56 +0430

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   devise_for :users
+  resources :general_shopping_lists, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :public_recipes  
   post 'toggle_public', to: 'recipes#toggle'
   # Defines the root path route ("/")
   root to: 'users#index'
-  get '/general_shopping_list', to: 'shopping_list#index'
 end
