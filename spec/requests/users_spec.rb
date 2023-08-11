1ca6d3b922441e1d3d74e3e5b293c4df2a4c41b8
Benson <njugunab655@gmail.com>
2023-08-11 10:54:36 +0300

    Test user index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should show a welcome message for non-logged in users' do
+      get root_path
+      expect(response.body).to include('Welcome')
+    end
   end
 end
