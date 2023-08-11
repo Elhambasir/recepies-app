36094b254ce611a9fa21607e6c62d57e7b297f58
Benson <njugunab655@gmail.com>
2023-08-11 14:29:00 +0300

    REwrite models specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
-  end
+  context 'when creating a user' do
+    let (:user) { build :user }
+    let (:user1) { build :user, email: nil }
+    let (:user2) { build :user, name: nil }
 
-  it "is not valid without a name" do
-    user = FactoryBot.build(:user, name: nil)
-    expect(user).not_to be_valid
-  end
-
-  it "is not valid without an email" do
-    user = FactoryBot.build(:user, email: nil)
-    expect(user).not_to be_valid
+    it 'should be a valid user with all attributes' do
+      expect(user.valid?).to eq(true)
+    end
+    it 'should be invalid without an email' do
+      expect(user1.valid?).to eq(false)
+    end
+    
+    it 'should be invalid without a name' do
+      expect(user2.valid?).to eq(false)
+    end
   end
 
   describe "associations" do
