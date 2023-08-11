5b2a6697ce7bc29a1e2fc8dd1baf3147d7c0f945
Elhambasir <elham1378basir@gmail.com>
2023-08-11 16:27:22 +0430

    Update general_shopping_lists_controller.rb

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,19 +1,24 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @foods = Food.joins(:recipe_foods)
-      .where(foods: { user_id: current_user.id })
-      .where('foods.quantity < recipe_foods.quantity')
-      .distinct
-    @food_lists = @foods.map do |food|
-      {
-        name: food.name,
-        measurement_unit: food.measurement_unit,
-        quantity: food.quantity,
-        total_quantity: food.recipe_foods.sum(:quantity) - food.quantity,
-        price: (food.recipe_foods.sum(:quantity) - food.quantity) * food.price
-      }
+    if user_signed_in?
+      @foods = Food.joins(:recipe_foods)
+        .where(foods: { user_id: current_user.id })
+        .where('foods.quantity < recipe_foods.quantity')
+        .distinct
+      @food_lists = @foods.map do |food|
+        {
+          name: food.name,
+          measurement_unit: food.measurement_unit,
+          quantity: food.quantity,
+          total_quantity: food.recipe_foods.sum(:quantity) - food.quantity,
+          price: (food.recipe_foods.sum(:quantity) - food.quantity) * food.price
+        }
+      end
+      @total_price = total_price_of_missing_food(@food_lists)
+    else
+      redirect_to new_user_session_path
     end
-    @total_price = total_price_of_missing_food(@food_lists)
+
   end
 
   private
