4d0fdae390c6592eba60ed8d142c2560dc58a414
Elhambasir <elham1378basir@gmail.com>
2023-08-11 17:10:23 +0430

    Create general_shopping_lists_spec.rb

---
+++ spec/requests/general_shopping_lists_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'GeneralShoppingLists', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+        get '/general_shopping_lists/'
+        expect(response.body).to include('You are being')
+    end
+  end
+end
